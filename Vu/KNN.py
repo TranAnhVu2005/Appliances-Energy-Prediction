720d5dbd9192a2eca4108b255c241d0166c59a09
TranAnhVu2005 <trananhvu314159@gmail.com>
2026-04-17 08:39:07 +0700

    complete LinearRegression

--- Vu/KNN.py
+++ Vu/KNN.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import KFold
 import numpy as np
 
 
@@ -19,49 +20,52 @@ X = energydata.drop(["Appliances", "date", "rv1",
 Y = energydata["Appliances"]
 # print(X)
 
+# # Phân chia tập dữ liệu theo nghi thức hold-out 2/3 1/3
+# # Vì muốn kiểm tra dữ liệu từ trên xuống dưới nên không dùng randomstate nữa
+# X_train, X_test, Y_train, Y_test = train_test_split(
+#     X, Y, test_size=1/3, random_state=42, shuffle=True)
 
-# Phân chia tập dữ liệu theo nghi thức hold-out 2/3 1/3
-# Vì muốn kiểm tra dữ liệu từ trên xuống dưới nên không dùng randomstate nữa
-X_train, X_test, Y_train, Y_test = train_test_split(
-    X, Y, test_size=1/3, random_state=42, shuffle=True)
+# # # Khởi tạo scaler
+# scaler = MinMaxScaler()
+# X_train_scaler = scaler.fit_transform(X_train)
+# X_test_scaler = scaler.transform(X_test)
 
-# # Khởi tạo scaler
-scaler = MinMaxScaler()
-X_train_scaler = scaler.fit_transform(X_train)
-X_test_scaler = scaler.transform(X_test)
+# # # Khởi tạo KNN hồi quy
+# k_values = range(1, 60, 2)
+# kBestForMAE = 1
+# kBestForMSE = 1
+# kBestForRMSE = 1
 
-# # Khởi tạo KNN hồi quy
-k_values = range(1, 60, 2)
-kBestForMAE = 1
-kBestForMSE = 1
-kBestForRMSE = 1
+# minMAE = 999999
+# minMSE = 999999
+# minRMSE = 999999
 
-minMAE = 999999
-minMSE = 999999
-minRMSE = 999999
+# for k in k_values:
+#     modelKNN = KNeighborsRegressor(n_neighbors=k, p=2)
+#     modelKNN.fit(X_train_scaler, Y_train)
+#     Y_pred = modelKNN.predict(X_test_scaler)
+#     mae = mean_absolute_error(Y_test, Y_pred)
+#     mse = mean_squared_error(Y_test, Y_pred)
+#     rmse = np.sqrt(mse)
+#     if (mae < minMAE):
+#         minMAE = mae
+#         kBestForMAE = k
+#     if (mse < minMSE):
+#         minMSE = mse
+#         kBestForMSE = k
+#     if (rmse < minRMSE):
+#         minRMSE = rmse
+#         kBestForRMSE = k
 
-for k in k_values:
-    modelKNN = KNeighborsRegressor(n_neighbors=k, p=2)
-    modelKNN.fit(X_train_scaler, Y_train)
-    Y_pred = modelKNN.predict(X_test_scaler)
-    mae = mean_absolute_error(Y_test, Y_pred)
-    mse = mean_squared_error(Y_test, Y_pred)
-    rmse = np.sqrt(mse)
-    if (mae < minMAE):
-        minMAE = mae
-        kBestForMAE = k
-    if (mse < minMSE):
-        minMSE = mse
-        kBestForMSE = k
-    if (rmse < minRMSE):
-        minRMSE = rmse
-        kBestForRMSE = k
+# print(f"Min Mean Absolute Error: {minMAE}")
+# print(f"K best for MAE: {kBestForMAE}")
 
-print(f"Min Mean Absolute Error: {minMAE}")
-print(f"K best for MAE: {kBestForMAE}")
+# print(f"Min Mean Square Error: {minMSE}")
+# print(f"K best for MSE: {kBestForMSE}")
 
-print(f"Min Mean Square Error: {minMSE}")
-print(f"K best for MSE: {kBestForMSE}")
+# print(f"Min Root Mean Square Error: {minRMSE}")
+# print(f"K best for RMSE: {kBestForRMSE}")
 
-print(f"Min Root Mean Square Error: {minRMSE}")
-print(f"K best for RMSE: {kBestForRMSE}")
+
+# Phân chia dữ liệu với k fold
+Đi 
---
+++ Vu/LinearRegression.py
@@ -0,0 +1,69 @@
+from pandas import read_csv
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+energydata = read_csv("energydata_complete.csv", delimiter=",")
+energydata['date'] = pd.to_datetime(energydata['date'])
+energydata['Hour'] = energydata['date'].dt.hour
+energydata['DayOfWeek'] = energydata['date'].dt.dayofweek
+
+# Tách đặc trưng
+X = energydata.drop(["Appliances", "date", "rv1",
+                    "rv2", "Unnamed: 29"], axis=1)
+# X = energydata.drop(["Appliances", "date", "Unnamed: 29"], axis=1)
+Y = energydata["Appliances"]
+# print(X)
+
+
+# Phân chia tập dữ liệu theo nghi thức hold-out 2/3 1/3
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=1/3, random_state=42, shuffle=True)
+
+# Hiển thị dữ liệu trực quan
+print(energydata.head())
+# Vẽ biểu đồ phân tán
+plt.scatter(energydata.lights, energydata.Appliances)
+plt.show()
+
+# # Khởi tạo scaler
+scaler = MinMaxScaler()
+X_train_scaler = scaler.fit_transform(X_train)
+X_test_scaler = scaler.transform(X_test)
+
+# Khởi tạo mô hình
+model = LinearRegression()
+model.fit(X_train_scaler, Y_train)
+Y_pred = model.predict(X_test_scaler)
+
+print("Các thuộc tính:", X_train.columns.values)
+print("Hệ số của các thuộc tính:", model.coef_)
+print("Bias (intercept):", model.intercept_)
+
+# Tính MSE
+mse = mean_squared_error(Y_test, Y_pred)
+
+# Tính rmse
+rmse = math.sqrt(mse)
+
+# TÍnh R2
+r2 = r2_score(Y_test, Y_pred)
+
+print("R2:", r2)
+print("MSE:", mse)
+print("RMSE:", rmse)
+
+plt.figure(figsize=(8, 5))
+plt.scatter(Y_test, Y_pred, alpha=0.3)
+plt.plot([Y_test.min(), Y_test.max()],
+         [Y_test.min(), Y_test.max()], 'r--')
+plt.xlabel("Actual")
+plt.ylabel("Predicted")
+plt.title("Actual vs Predicted")
+plt.show()
