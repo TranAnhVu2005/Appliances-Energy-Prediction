a2a6bf3f0e775d502c23bd80a7a13936905a8aaf
TranAnhVu2005 <trananhvu314159@gmail.com>
2026-04-18 19:46:09 +0700

    intro dataset

--- TongHop/IntroDataset.py
+++ TongHop/IntroDataset.py
@@ -1,20 +1,52 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_absolute_error
+import matplotlib.pyplot as plt
+
+# Load dữ liệu
 df = pd.read_csv(
     "D:/All/Information Technology  - CTU/CurrentSemester/CT294 - Applied machine learning/MayHocUngDung_TeamWork/Appliances-Energy-Prediction/appliances+energy+prediction/energydata_complete.csv", sep=",")
+df['date'] = pd.to_datetime(df['date'])
+df['Hour'] = df['date'].dt.hour
+df['DayOfWeek'] = df['date'].dt.dayofweek
 
 
-# Thiết lập kích thước khung hình
-plt.figure(figsize=(10, 6))
+# Tiền xử lý
+X = df.drop(["Appliances", "date", "rv1",
+             "rv2"], axis=1)
 
-# Vẽ biểu đồ Histogram kết hợp đường cong mật độ (KDE) cho nhãn Appliances
-sns.histplot(df['Appliances'], bins=50, kde=True, color='blue')
-# Thêm tiêu đề
-plt.title('Phân phối lượng điện tiêu thụ của thiết bị (Appliances)')
-plt.xlabel('Lượng điện tiêu thụ (Wh)')
-plt.ylabel('Tần suất (Số lượng dòng)')
 
-# Hiển thị biểu đồ
-plt.show()
+Y = df["Appliances"]
+# Train/Test split
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=0.2, shuffle=False
+)
+
+# Cho train
+sample = X_train.iloc[:, :10].sample(10, random_state=42)
+samplelabel = Y_train.loc[sample.index]
+
+# scale hoặc không, comment hoặc gỡ comment dòng này
+scaler = MinMaxScaler()
+sample = pd.DataFrame(
+    scaler.fit_transform(sample),
+    columns=sample.columns,
+    index=sample.index
+)
+
+sample.insert(0, "Appliances", samplelabel)
+# print(sample)
+
+# Cho test
+sampleTest = X_test.iloc[:, :10].sample(2, random_state=42)
+# scale hoặc không, comment hoặc gỡ comment dòng này
+sampleTest = pd.DataFrame(
+    scaler.transform(sampleTest),
+    columns=sampleTest.columns,
+    index=sampleTest.index
+)
+print(sampleTest)
